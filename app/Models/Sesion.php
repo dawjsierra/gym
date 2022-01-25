a10ca842f0963630339493ba12d83d9594181075
dawjsierra <sierra.javier.daw2@gmail.com>
2022-01-25 16:32:14 +0000

    clase 2501

---
+++ app/Models/Sesion.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sesion extends Model
+{
+    use HasFactory;
+    /*$fecha->format('d-m-Y');
+
+    //añadir al modelo este atributo
+    //columnas a indicar, las que son fechas
+    //conversion automatica de la bbdd
+    protected $dates = ['date'];
+
+    $day = \Carbon\Carbon::dateFormat('2022-1-1');
+    $days = $day->daysInMonth +1
+
+    //buscar en google "laravel foreach days in month*/
+
+    /* buscar info de Laravel Carbon */
+
+}
---
+++ database/migrations/2022_01_25_152808_create_sesions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSesionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sesions', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('horaInicio');
+            $table->dateTime('horaFin');
+            $table->timestamps();
+            $table->unsignedBigInteger('act_id')->default(1);
+            $table->foreign('act_id')->references('id')->on('activities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sesions');
+    }
+}
